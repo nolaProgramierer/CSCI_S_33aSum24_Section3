a1775575e1e9e08e5b3c155f72904d8991351039
Glenn Langdon <glennlangdon@gmail.com>
2024-07-09 15:53:07 -0500

    initial commit

---
+++ exercise_sorted.py
@@ -0,0 +1,24 @@
+# sorted(iterable, key=None, reverse=False)
+
+nums = (43, 2, 99, 33, -4, 20)
+
+composers = ["Beethoven", "Ravel", "Brahms", "Debussy", "Mahler", "Bruckner", "Mozart", "Bach", "Stravinsky"]
+
+cars = [
+    {"brand": "BMW", "trim": "328i", "color": "gray", "price": 38900},
+     {"brand": "Mercedes", "trim": "350E", "color": "white", "price": 89000},
+      {"brand": "Porsche", "trim": "911", "color": "orange", "price": 95000},
+       {"brand": "Kia", "trim": "Sorento", "color": "blue", "price": 30900},
+        {"brand": "Toyota", "trim": "Sienna", "color": "gray", "price": 51200},
+]
+
+# Sort numbers
+
+
+# Sort numbers in reverse
+
+
+# Sort composers by word length in reverse
+
+
+# Sort cars by price (ascending)
